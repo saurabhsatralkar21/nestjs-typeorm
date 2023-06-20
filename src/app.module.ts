2358fb2bb39812994dc144553c85b9c91fa70558
Saurabh S <saurabh.satralkar7@gmail.com>
2023-06-20 18:39:46 +0200

    NestJS app for typeorm practise

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+    entities: ['dist/**/*.entity.js'],
+    synchronize: true,
+  }), UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
