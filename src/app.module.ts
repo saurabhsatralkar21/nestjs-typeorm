53105bec382f1c3f257568e66cad2195117baa1a
Saurabh S <saurabh.satralkar7@gmail.com>
2023-07-05 21:48:34 +0200

    Migration in progress

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,20 @@ import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { DataSourceOptions, DataSourceOptions } from 'typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
-    entities: ['dist/**/*.entity.js'],
+  imports: [TypeOrmModule.forRoot(
+    // type: 'postgres',
+    // host: 'localhost',
+    // port: 5432,
+    // username: 'postgres',
+    // password: 'postgres',
+    // database: 'postgres',
+    // entities: ['dist/**/*.entity.js'],
     // synchronize: true,
-  }), UsersModule],
+    DataSourceOptions
+  ), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
